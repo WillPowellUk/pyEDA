5dd6ae277033b1f78358b8ccc456704a0ddc5b73
Will Powell <me@willpowell.uk>
2024-07-01 17:02:04 +0100

    black formatted

--- pyEDA/filtering.py
+++ pyEDA/filtering.py
@@ -1,16 +1,18 @@
 # Importing necessary libraries
 from scipy.signal import butter, filtfilt
-  
-'''
+
+"""
 Low pass filter to remove noise specially artifact noise
-'''
+"""
+
+
 def butter_lowpassfilter(data, cutoff, sample_rate, order=2):
-  '''standard lowpass filter.
+    """standard lowpass filter.
     Function that filters the data using standard Butterworth lowpass filter
-	
+
     Parameters
     ----------
-	data : 1-d array
+        data : 1-d array
         array containing the gsr data
     cutoff : int or float
         frequency in Hz that acts as cutoff for filter.
@@ -20,14 +22,14 @@ def butter_lowpassfilter(data, cutoff, sample_rate, order=2):
         filter order, defines the strength of the roll-off
         around the cutoff frequency.
         default: 2
-    
+
     Returns
     -------
     y : 1-d array
         filtered gsr data
-  '''
-  nyq = 0.5 * sample_rate
-  normal_cutoff = cutoff/nyq
-  b, a = butter(order, normal_cutoff, btype='low', analog=False)
-  y = filtfilt(b, a, data)
-  return y
+    """
+    nyq = 0.5 * sample_rate
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype="low", analog=False)
+    y = filtfilt(b, a, data)
+    return y
